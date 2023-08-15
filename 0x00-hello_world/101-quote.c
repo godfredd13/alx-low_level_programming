714d7144bd1282958c45181049482c69107f1b3f
cusink859 <godfredagyapong7@yahoo.com>
2023-08-14 21:20:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm-intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C program that prints a line to thr standard error
+ * return:  (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", stdout);
+return (1);;
+}
